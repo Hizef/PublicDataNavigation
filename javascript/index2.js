1ad083eb5c3e22791db99b822def3b17e70d794a
JiHwan Kim <earth7923@gmail.com>
2024-03-16 22:23:15 +0900

    2nd commit

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,10 +1,1 @@
-const searchBtn = document.getElementById("searchBtn");
-const searchInput = document.getElementById("searchInput");
 
-searchBtn.addEventListener("click", () => {
-  if (searchInput.style.display === "none") {
-    searchInput.style.display = "block";
-  } else {
-    searchInput.style.display = "none";
-  }
-});
